810b300a6e6fd93c956da65755021be78b7f6bdb
Devanshi Prasad <devanshiprasad@outlook.com>
2024-07-25 19:45:40 +0530

    added reset and pastel color button

--- Color flipper/index.js
+++ Color flipper/index.js
@@ -1,7 +1,25 @@
 const hex= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color= document.querySelector(".color");
+const reset= document.getElementById("reset");
+const pastel= document.getElementById("pastel");
 
+
+const originalText= color.textContent;
+const originalBg = document.body.style.backgroundColor;
+
+reset.addEventListener("click",function(){
+    color.textContent= originalText;
+    document.body.style.backgroundColor= originalBg;
+})
+
+pastel.addEventListener("click",function(){
+    const pastelColor = getPastelColor();
+    //console.log(pastelColor);
+
+    color.textContent = pastelColor;
+    document.body.style.backgroundColor = pastelColor;
+})
 btn.addEventListener("click",function(){
     let hexColor="#";
     for(let i=0;i<6;i++){
@@ -15,4 +33,11 @@ btn.addEventListener("click",function(){
 
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length);
-  }
+}
+
+function getPastelColor() {
+    const r = Math.floor((Math.random() * 127) + 127); // Random value between 127 and 254
+    const g = Math.floor((Math.random() * 127) + 127); // Random value between 127 and 254
+    const b = Math.floor((Math.random() * 127) + 127); // Random value between 127 and 254
+    return `rgb(${r}, ${g}, ${b})`;
+}
